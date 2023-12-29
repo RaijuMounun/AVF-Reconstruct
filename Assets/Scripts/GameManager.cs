7033962d5fcf4bc150409b228d785c309bbddb27
Eren Keskinoğlu <87018234+RaijuMounun@users.noreply.github.com>
2023-12-29 14:18:54 +0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,32 +6,23 @@ using Sirenix.OdinInspector;
 using UnityEditor;
 
 
-public class GameManager : MonoBehaviour //I thought I needed this more but...
+public class GameManager : MonoBehaviour 
 {
     #region Variables
 
-    [FoldoutGroup("Scripts")] public GameManager gm; //The reason I didn't use static here is I had a problem with static, but I don't know why and I did this, problem solved.
+    [FoldoutGroup("Scripts")] public GameManager gm; 
     [FoldoutGroup("Scripts")] public SO_Manager SOM;
-    public bool fullReset = false;
     public float money;
     #endregion
 
-    private void Awake()
-    {
-        gm = this;
-    }
+    private void Awake() => gm = this;
 
-    private void Update()
+    public void SetFullReset()
     {
-        if (fullReset == true)
+        for (int i = 0; i < 8; i++)
         {
-            for (int i = 0; i < 8; i++)
-            {
-                SOM.objectsList[i].FullReset();
-            }
-            fullReset = false;
+            SOM.objectsList[i].FullReset();
         }
-            
-
+        fullReset = false;
     }
 }
